d32ca6b3ed03f847c7b032227f243014e28ba8e2
MeghanaBait <133511757+MeghanaBait@users.noreply.github.com>
2023-12-24 10:20:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+	if(year%4 === 0){
+		if(year % 100 === 0 && year % 400 !== 0){
+			return 365;
+		}else{
+			return 366;
+		}
+	}else{
+		return 365;
+	}
+}
+
+const year = prompt("Enter Year: ");
+alert(daysOfAYear(year));
